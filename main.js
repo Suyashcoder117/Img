6039663210c9029832d22cde7c55fedc67c52213
Suyashcoder117 <75789654+Suyashcoder117@users.noreply.github.com>
2021-07-14 17:14:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,31 @@ camea = document.getElementById("camera");
 Webcam.set({
     width: 345,
     height: 298,
-      
+    image_format: 'png',
+    png_quality: 90
      
  });
+ Webcam.attach(camea);
+ function capture(){
+     Webcam.snap(function(data_uri) {
+         document.getElementById("snapshot").innerHTML = "<img id='captured_img' src='" + data_uri +"' />";
+         ;});
+     }
+console.log("ml5 version", ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Ce9enb-lw/model.json');
+function modelLoaded(){
+    console.log("model loaded");
+}
+function identify(){
+    img = document.getElementById('captured_img');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+if (error){
+    console.error(error);
+} else {
+    console.log(result);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+}
+}
